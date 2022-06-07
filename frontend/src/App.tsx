3cbbf3518fad09dba66af3208a7755453568cc1f
AlexRequelme <alexdeveloper09@gmail.com>
2022-06-07 16:26:10 -0500

    Added basic react config

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+
+function App() {
+    return (
+        <div className="bg-red-200">
+            <p>Works!!!</p>
+        </div>
+    );
+}
+
+export default App;
